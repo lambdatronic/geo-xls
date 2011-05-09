6eb8eb2b615cda5d61e5b844499ef70a4556ce4c
Gary Johnson <gwjohnso@uvm.edu>
2011-05-09 16:50:41 -0400

    Add exception check for run-gdal-info.

--- src/geo_xls/core.clj
+++ src/geo_xls/core.clj
@@ -229,50 +229,51 @@
 
 (defn extract-georeferences
   [geoserver-data-dir uri]
-  (let [gdal-info          (run-gdal-info geoserver-data-dir uri)
-        cols-rows-regex    #"(?s)Size is (\d+), (\d+)"
-        pixel-size-regex   #"(?s)Pixel Size = \(([\-\.0123456789]+),([\-\.0123456789]+)\)"
-        origin-regex       #"(?s)Origin = \(([\-\.0123456789]+),([\-\.0123456789]+)\)"
+  (if-let [gdal-info (run-gdal-info geoserver-data-dir uri)]
+    (let [cols-rows-regex    #"(?s)Size is (\d+), (\d+)"
+          pixel-size-regex   #"(?s)Pixel Size = \(([\-\.0123456789]+),([\-\.0123456789]+)\)"
+          origin-regex       #"(?s)Origin = \(([\-\.0123456789]+),([\-\.0123456789]+)\)"
 
-        color-interp-regex #"(?s)ColorInterp=(\w+)"
-        native-crs-regex   #"(?s)Coordinate System is:\s*\n(.+)Origin"
+          color-interp-regex #"(?s)ColorInterp=(\w+)"
+          native-crs-regex   #"(?s)Coordinate System is:\s*\n(.+)Origin"
 
-        upper-left-regex   #"(?s)Upper Left\s+\(\s*([\-\.0123456789]+),\s+([\-\.0123456789]+)\)\s+\(\s*([^,]+),\s*([^\)]+)\)"
-        lower-left-regex   #"(?s)Lower Left\s+\(\s*([\-\.0123456789]+),\s+([\-\.0123456789]+)\)\s+\(\s*([^,]+),\s*([^\)]+)\)"
-        upper-right-regex  #"(?s)Upper Right\s+\(\s*([\-\.0123456789]+),\s+([\-\.0123456789]+)\)\s+\(\s*([^,]+),\s*([^\)]+)\)"
-        lower-right-regex  #"(?s)Lower Right\s+\(\s*([\-\.0123456789]+),\s+([\-\.0123456789]+)\)\s+\(\s*([^,]+),\s*([^\)]+)\)"
+          upper-left-regex   #"(?s)Upper Left\s+\(\s*([\-\.0123456789]+),\s+([\-\.0123456789]+)\)\s+\(\s*([^,]+),\s*([^\)]+)\)"
+          lower-left-regex   #"(?s)Lower Left\s+\(\s*([\-\.0123456789]+),\s+([\-\.0123456789]+)\)\s+\(\s*([^,]+),\s*([^\)]+)\)"
+          upper-right-regex  #"(?s)Upper Right\s+\(\s*([\-\.0123456789]+),\s+([\-\.0123456789]+)\)\s+\(\s*([^,]+),\s*([^\)]+)\)"
+          lower-right-regex  #"(?s)Lower Right\s+\(\s*([\-\.0123456789]+),\s+([\-\.0123456789]+)\)\s+\(\s*([^,]+),\s*([^\)]+)\)"
 
-        [cols rows]                (rest (re-find cols-rows-regex  gdal-info))
-        [pixel-width pixel-height] (rest (re-find pixel-size-regex gdal-info))
-        [x-origin y-origin]        (rest (re-find origin-regex     gdal-info))
+          [cols rows]                (rest (re-find cols-rows-regex  gdal-info))
+          [pixel-width pixel-height] (rest (re-find pixel-size-regex gdal-info))
+          [x-origin y-origin]        (rest (re-find origin-regex     gdal-info))
 
-        [ul-native-x ul-native-y ul-latlon-x ul-latlon-y] (rest (re-find upper-left-regex gdal-info))
-        [ll-native-x ll-native-y ll-latlon-x ll-latlon-y] (rest (re-find lower-left-regex gdal-info))
-        [ur-native-x ur-native-y ur-latlon-x ur-latlon-y] (rest (re-find upper-right-regex gdal-info))
-        [lr-native-x lr-native-y lr-latlon-x lr-latlon-y] (rest (re-find lower-right-regex gdal-info))
+          [ul-native-x ul-native-y ul-latlon-x ul-latlon-y] (rest (re-find upper-left-regex gdal-info))
+          [ll-native-x ll-native-y ll-latlon-x ll-latlon-y] (rest (re-find lower-left-regex gdal-info))
+          [ur-native-x ur-native-y ur-latlon-x ur-latlon-y] (rest (re-find upper-right-regex gdal-info))
+          [lr-native-x lr-native-y lr-latlon-x lr-latlon-y] (rest (re-find lower-right-regex gdal-info))
 
-        [ul-native-x ul-native-y] (map read-string [ul-native-x ul-native-y])
-        [ll-native-x ll-native-y] (map read-string [ll-native-x ll-native-y])
-        [ur-native-x ur-native-y] (map read-string [ur-native-x ur-native-y])
-        [lr-native-x lr-native-y] (map read-string [lr-native-x lr-native-y])]
+          [ul-native-x ul-native-y] (map read-string [ul-native-x ul-native-y])
+          [ll-native-x ll-native-y] (map read-string [ll-native-x ll-native-y])
+          [ur-native-x ur-native-y] (map read-string [ur-native-x ur-native-y])
+          [lr-native-x lr-native-y] (map read-string [lr-native-x lr-native-y])]
 
-    {:cols-rows    (str cols " " rows)
-     :pixel-width  pixel-width
-     :pixel-height pixel-height
-     :x-origin     x-origin
-     :y-origin     y-origin
-     :color-interp (second (re-find color-interp-regex gdal-info))
-     :nativeCRS    (second (re-find native-crs-regex gdal-info))
-     :native-min-x (str (min ul-native-x ll-native-x))
-     :native-max-x (str (max ur-native-x lr-native-x))
-     :native-min-y (str (min ll-native-y lr-native-y))
-     :native-max-y (str (max ul-native-y ur-native-y))
-     :latlon-min-x (str (apply min (map dms->dd [ul-latlon-x ll-latlon-x])))
-     :latlon-max-x (str (apply max (map dms->dd [ur-latlon-x lr-latlon-x])))
-     :latlon-min-y (str (apply min (map dms->dd [ll-latlon-y lr-latlon-y])))
-     :latlon-max-y (str (apply max (map dms->dd [ul-latlon-y ur-latlon-y])))
-     :shear-x      (str (radians->degrees (Math/asin (/ (- ll-native-x ul-native-x) (- ul-native-y ll-native-y)))))
-     :shear-y      (str (radians->degrees (Math/asin (/ (- ur-native-y ul-native-y) (- ur-native-x ul-native-x)))))}))
+      {:cols-rows    (str cols " " rows)
+       :pixel-width  pixel-width
+       :pixel-height pixel-height
+       :x-origin     x-origin
+       :y-origin     y-origin
+       :color-interp (second (re-find color-interp-regex gdal-info))
+       :nativeCRS    (second (re-find native-crs-regex gdal-info))
+       :native-min-x (str (min ul-native-x ll-native-x))
+       :native-max-x (str (max ur-native-x lr-native-x))
+       :native-min-y (str (min ll-native-y lr-native-y))
+       :native-max-y (str (max ul-native-y ur-native-y))
+       :latlon-min-x (str (apply min (map dms->dd [ul-latlon-x ll-latlon-x])))
+       :latlon-max-x (str (apply max (map dms->dd [ur-latlon-x lr-latlon-x])))
+       :latlon-min-y (str (apply min (map dms->dd [ll-latlon-y lr-latlon-y])))
+       :latlon-max-y (str (apply max (map dms->dd [ul-latlon-y ur-latlon-y])))
+       :shear-x      (str (radians->degrees (Math/asin (/ (- ll-native-x ul-native-x) (- ul-native-y ll-native-y)))))
+       :shear-y      (str (radians->degrees (Math/asin (/ (- ur-native-y ul-native-y) (- ur-native-x ul-native-x)))))})
+    (throw (Exception. (str "run-gdal-info failed with geoserver-data-dir=" geoserver-data-dir " and uri=" uri)))))
 
 (defn create-coverage
   [{:keys [geoserver-data-dir]} {:keys [Workspace Store Layer Description URI NativeSRS DeclaredSRS]}]
